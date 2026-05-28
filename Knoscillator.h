5201491825e9d3a38854d3edf051470711bd2d87
Dami Quartz <damien@compartmental.net>
2026-05-27 19:18:29 -0700

    remove noise table code from knoscillator

--- Knoscillator.h
+++ Knoscillator.h
@@ -28,10 +28,6 @@ private:
   using q31_t = vessl::q31;
   using q31_p = vessl::param<q31_t>;
   
-  static constexpr size_t noiseDim = 128;
-  static constexpr float  noiseStep = 4.0f / noiseDim;
-  
-  using NoiseTable = vessl::sample::wavetable<float, noiseDim*noiseDim>;
 
 public:
   KnotOscil      knoscil;
@@ -51,12 +47,12 @@ public:
     analog_p squiggleAmt;
     analog_p noiseAmt;
   } params;
-  
-  NoiseTable noiseTable;
 
   explicit Knoscillator(float sr)
     : knoscil(sr), rotator(sr)
-    , dt(vessl::cast<phase_t>(1.0f/sr)), phaseMod(vessl::phase_zero), phaseS(vessl::phase_zero)
+    , dt(vessl::cast<phase_t>(1.0f/sr))
+    , phaseMod(vessl::phase_zero)
+    , phaseS(vessl::phase_zero)
   {
     knoscil.knotP() = 2.f;
     knoscil.knotQ() = 1.f;
@@ -66,21 +62,9 @@ public:
     
     params.fmRatio.value = 2;
     params.fmIndex.value = 0.f;
-    
-    // for (size_t x = 0; x < noiseDim; ++x)
-    // {
-    //   for (size_t y = 0; y < noiseDim; ++y)
-    //   {
-    //     size_t i = x * noiseDim + y;
-    //     noiseTable.set(i, vessicle::perlin2d(x*noiseStep, y*noiseStep, 1, 4) * 2 - 1);
-    //   }
-    // }
   }
   
 public:
-  SampleType knotCoord;
-  SampleType knotCoordRotated;
-
   [[nodiscard]] VESSL_INLINE const KnotOscil& knot() const { return knoscil; }
   
   [[nodiscard]] VESSL_INLINE param knotTypeA() const { return knoscil.knotTypeA(); }
@@ -118,7 +102,6 @@ public:
   VESSL_INLINE SampleType generate() override
   {
     //analog_t sVol = params.squiggleAmt.value * 0.25f;
-    //analog_t nVol = params.noiseAmt.value * 0.5f;
     
     analog_t freq = params.freqInHz.value;
     // phase modulate in sync with the current frequency
@@ -136,10 +119,8 @@ public:
     coord = rotator.process(coord);
     
     // phase_t st = phaseS + fm;
-    // analog_t nz = nVol * noise(coord.x, coord.y);
-    // coord.x += vessl::math::cos<analog_t>(st)*sVol + coord.x * nz;
-    // coord.y += vessl::math::sin<analog_t>(st)*sVol + coord.y * nz;
-    // coord.z += coord.z * nz;
+    // coord.x += vessl::math::cos<analog_t>(st)*sVol;
+    // coord.y += vessl::math::sin<analog_t>(st)*sVol;
     
     // q31_t fInc = q31_t((int64_t)(freq * dt));
     // analog_t knotP = knoscil.knotP().readAnalog();
@@ -159,7 +140,6 @@ public:
   VESSL_INLINE void generate(vessl::array<SampleType> dest)
   {
     // float sVol = params.squiggleAmt.value * 0.25f;
-    // float nVol = params.noiseAmt.value * 0.5f;
     
     analog_t freq = params.freqInHz.value;
     // phase modulate in sync with the current frequency
@@ -209,14 +189,5 @@ protected:
       rotationX(), rotationY(), rotationZ()
     };
     return p[index];
-  }
-  
-private:
-  [[nodiscard]] VESSL_INLINE float noise(float x, float y) const
-  {
-    size_t nx = static_cast<size_t>(vessl::math::abs(x) / noiseStep) % noiseDim;
-    size_t ny = static_cast<size_t>(vessl::math::abs(y) / noiseStep) % noiseDim;
-    size_t ni = nx * noiseDim + ny;
-    return noiseTable.get(ni);
   }
 };

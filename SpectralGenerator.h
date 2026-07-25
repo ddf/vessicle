e62db9dea67aa2e9c52c2ff5e02833ed45e5cec0
Damien Quartz <damien@compartmental.net>
2026-07-25 16:12:22 -0700

    update vessl, more clarity with SpectralGenerator allocations

--- SpectralGenerator.h
+++ SpectralGenerator.h
@@ -4,29 +4,32 @@
 #include "Window.h"
 
 // lightweight extension that allocates and deallocates the correct amount of memory.
-template<typename T, vessl::size_t SpectrumSize, vessl::size_t Overlap = 2>
-class SpectralGenerator : public vessl::generators::spectral<T, SpectrumSize, Overlap>
+template<typename T, vessl::size_t SpectrumSize>
+class SpectralGenerator : public vessl::generators::spectral<T, SpectrumSize>
 {
-  using Data = typename vessl::generators::spectral<T, SpectrumSize, Overlap>::data;
-  using FrequencyBandType = typename vessl::generators::spectral<T, SpectrumSize, Overlap>::frequency_band;
-  using SampleType = typename vessl::generators::spectral<T, SpectrumSize, Overlap>::sample_t;
-  using ComplexType = typename vessl::generators::spectral<T, SpectrumSize, Overlap>::complex_t;
+  using Data = typename vessl::generators::spectral<T, SpectrumSize>::data;
+  using FrequencyBandType = typename vessl::generators::spectral<T, SpectrumSize>::frequency_band;
+  using SampleType = typename vessl::generators::spectral<T, SpectrumSize>::sample_t;
+  using ComplexType = typename vessl::generators::spectral<T, SpectrumSize>::complex_t;
   
   SpectralGenerator(Data& data, vessl::analog_t sample_rate)
-    : vessl::generators::spectral<T, SpectrumSize, Overlap>(data, sample_rate)
+    : vessl::generators::spectral<T, SpectrumSize>(data, sample_rate)
   {
   }
   
 public:
   static SpectralGenerator* create(vessl::analog_t sample_rate, vessl::sample::windows::type window_type)
   {
-    constexpr vessl::size_t bands = SpectrumSize/2;
+    constexpr vessl::size_t bands_size = SpectrumSize/2;
+    FrequencyBandType* frequency_data = new FrequencyBandType[bands_size];
+    ComplexType* spectrum_data = new ComplexType[bands_size];
+    SampleType* sample_data = new SampleType[SpectrumSize*2];
+    SampleType* window_data = new SampleType[SpectrumSize];
     Data data = {
-      vessl::array<FrequencyBandType>(new FrequencyBandType[bands], bands),
-      vessl::array<ComplexType>(new ComplexType[SpectrumSize], SpectrumSize),
-      vessl::array<SampleType>(new SampleType[SpectrumSize], SpectrumSize),
-      vessl::array<SampleType>(new SampleType[SpectrumSize], SpectrumSize),
-      vessl::sample::ring_buffer<SampleType>(new SampleType[SpectrumSize*8], SpectrumSize*8)
+      vessl::array<FrequencyBandType>(frequency_data, bands_size), // frequencies
+      vessl::array<ComplexType>(spectrum_data, bands_size), // spectrum
+      vessl::array<SampleType>(sample_data, SpectrumSize*2), // signal
+      vessl::array<SampleType>(window_data, SpectrumSize), // window
     };
     
     //Window::triangular(data.window.data(), data.window.size());
@@ -39,9 +42,9 @@ public:
   {
     if (generator)
     {
-      delete[] generator->buffer_.data();
       delete[] generator->window_.data();
-      delete[] generator->signal_.data();
+      delete[] generator->signal_a_.data();
+      // shouldn't need to delete b because it was allocated along with a in create.
       delete[] generator->spectrum_.data();
       delete[] generator->frequencies_.data();
     }
